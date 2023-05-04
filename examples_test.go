4b647e007abe22a1486cabab9a3937a318e2eb37
Saeed Rasooli <saeed.gnu@gmail.com>
2023-05-04 06:07:51 +0330

    examples_test.go: use same package

--- examples_test.go
+++ examples_test.go
@@ -1,16 +1,14 @@
-package wordwrap_test
+package wordwrap
 
 import (
 	"fmt"
-
-	"github.com/ilius/go-wordwrap"
 )
 
 // Words are wrapped at whitespace and at hyphens. Wrapping whitespace is
 // trimmed, hyphens are kept. If a word is too long to fit in the limit, it
 // will be broken at the limit.
 func Example() {
-	lines := wordwrap.WordWrap("this test-string has been successfully wrapped successfully", 10)
+	lines := WordWrap("this test-string has been successfully wrapped successfully", 10)
 	for _, line := range lines {
 		fmt.Println(line)
 	}
@@ -26,7 +24,7 @@ func Example() {
 
 // Words are wrapped at spaces.
 func ExampleWordWrap_space() {
-	lines := wordwrap.WordWrap("we wrap at spaces", 9)
+	lines := WordWrap("we wrap at spaces", 9)
 	for _, line := range lines {
 		fmt.Println(line)
 	}
@@ -37,7 +35,7 @@ func ExampleWordWrap_space() {
 
 // Words are wrapped at hyphens, and the hyphens are kept at the ends of lines.
 func ExampleWordWrap_hyphen() {
-	lines := wordwrap.WordWrap("hyphenated-words", 15)
+	lines := WordWrap("hyphenated-words", 15)
 	for _, line := range lines {
 		fmt.Println(line)
 	}
@@ -48,7 +46,7 @@ func ExampleWordWrap_hyphen() {
 
 // Words that are too long for the limit will be broken.
 func ExampleWordWrap_long() {
-	lines := wordwrap.WordWrap("longwordsarewrapped", 10)
+	lines := WordWrap("longwordsarewrapped", 10)
 	for _, line := range lines {
 		fmt.Println(line)
 	}
